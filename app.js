650aab5c1e24e9539bccdf1cf17f787087ffd948
Phil <happy.melodist@gmail.com>
2021-11-13 17:14:56 -0600

    complete coding the dynamically stylled mousover cursor effects

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
+// const mouseCursor = document.querySelector(".cursor");
+// const navLinks = document.querySelectorAll(".nav-links li");
+
+// window.addEventListener("mousemove", cursor);
+
+// function cursor(e) {
+//   // console.log(e);
+//   mouseCursor.style.top = e.pageY + "px";
+//   mouseCursor.style.left = e.pageX + "px";
+// }
+
+// navLinks.forEach(function (link) {
+//   link.addEventListener("mouseout", function () {
+//     mouseCursor.classList.remove("link-grow");
+//     link.classList.remove("hovered-link");
+//   });
+
+//   link.addEventListener("mouseover", function () {
+//     mouseCursor.classList.add("link-grow");
+//     link.classList.add("hovered-link");
+//   });
+// });
+
+// my code below
+
 const mouseCursor = document.querySelector(".cursor");
 const navLinks = document.querySelectorAll(".nav-links li");
 
 window.addEventListener("mousemove", cursor);
 
 function cursor(e) {
-  // console.log(e);
+  console.log(e);
   mouseCursor.style.top = e.pageY + "px";
   mouseCursor.style.left = e.pageX + "px";
 }
 
-navLinks.forEach(function (link) {
-  link.addEventListener("mouseout", function () {
+navLinks.forEach((link) => {
+  link.addEventListener("mouseout", () => {
     mouseCursor.classList.remove("link-grow");
     link.classList.remove("hovered-link");
   });
-
-  link.addEventListener("mouseover", function () {
+  link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
     link.classList.add("hovered-link");
   });
